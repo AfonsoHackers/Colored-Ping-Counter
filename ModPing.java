00ddfec5b7c4f80365467477952fc5a40530bd67
AfonsoHackers <44532605+AfonsoHackers@users.noreply.github.com>
2020-03-07 17:20:38 +0100

    Update ModPing.java

--- ModPing.java
+++ ModPing.java
@@ -9,15 +9,10 @@ public class ModPing extends ModDraggable {
 	
 	private ScreenPosition pos;
 	
-
-	@Override
-	public int getWidth() {
-		int ping = mc.getMinecraft().getNetHandler().getPlayerInfo(mc.getMinecraft().thePlayer.getUniqueID()).getResponseTime();
-		return font.getStringWidth("Ping" + ping);
 		
 	}
-	# This is used to get the length of ping alone, without "Ping;"
-	public int getWidth2() {
+	@Override
+	public int getWidth() {
 		int ping = mc.getMinecraft().getNetHandler().getPlayerInfo(mc.getMinecraft().thePlayer.getUniqueID()).getResponseTime();
 		return font.getStringWidth("" + ping);
 		
@@ -34,22 +29,22 @@ public class ModPing extends ModDraggable {
 	public void render(ScreenPosition pos) {
 		int ping = mc.getMinecraft().getNetHandler().getPlayerInfo(mc.getMinecraft().thePlayer.getUniqueID()).getResponseTime();
 		if(ping <= 10) {
-			font.drawStringWithShadow("§b  " + ping + " ms", pos.getAbsoluteX() + getWidth2() + 3, pos.getAbsoluteY() + 4, -1);
+			font.drawStringWithShadow("§b  " + ping + " ms", pos.getAbsoluteX() + getWidth() + 3, pos.getAbsoluteY() + 4, -1);
 			return;
 		} else if (ping <= 50) {
-			font.drawStringWithShadow("§a  " + ping + " ms", pos.getAbsoluteX() + getWidth2(), pos.getAbsoluteY() + 4, -1);
+			font.drawStringWithShadow("§a  " + ping + " ms", pos.getAbsoluteX() + getWidth(), pos.getAbsoluteY() + 4, -1);
 			return;
 		} else if (ping <= 100) {
-			font.drawStringWithShadow("§2  " + ping + " ms", pos.getAbsoluteX() + getWidth2() - 3, pos.getAbsoluteY() + 4, -1);
+			font.drawStringWithShadow("§2  " + ping + " ms", pos.getAbsoluteX() + getWidth() - 3, pos.getAbsoluteY() + 4, -1);
 			return;
 		} else if (ping <= 150) {
-			font.drawStringWithShadow("§e  " + ping + " ms" , pos.getAbsoluteX() + getWidth2() - 3, pos.getAbsoluteY() + 4, -1);
+			font.drawStringWithShadow("§e  " + ping + " ms" , pos.getAbsoluteX() + getWidth() - 3, pos.getAbsoluteY() + 4, -1);
 			return;
 		} else if (ping <= 180) {
-			font.drawStringWithShadow("§c  " + ping + " ms", pos.getAbsoluteX() + getWidth2() - 3, pos.getAbsoluteY() + 4, -1);
+			font.drawStringWithShadow("§c  " + ping + " ms", pos.getAbsoluteX() + getWidth() - 3, pos.getAbsoluteY() + 4, -1);
 			return;
 		} else {
-			font.drawStringWithShadow("§4  " + ping + " ms", pos.getAbsoluteX() + getWidth2() - 3, pos.getAbsoluteY() + 4, -1);
+			font.drawStringWithShadow("§4  " + ping + " ms", pos.getAbsoluteX() + getWidth() - 3, pos.getAbsoluteY() + 4, -1);
 			return;
 		}
 		
